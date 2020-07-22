62b93c927149f799643e9d4c1ce00db6deaa1f79
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-22 16:57:41 -0300

    create component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as htmlPdf from 'html2pdf.js';
+
+import { FilmsService } from './services/films.service';
+
+import { Film } from './shared/film.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ng-html-to-pdf';
+  films: Film[];
+
+  constructor(private serviceFilms: FilmsService) {}
+
+  ngOnInit() {
+    this.films = this.serviceFilms.filmsList();
+  }
+
+  generatePdf() {
+    const options = {
+      filename: 'lista_de_filmes.pdf',
+      image: {type: 'png'},
+      html2Canvas: {},
+      jsPDF: {orientation: 'landscape'}
+    };
+
+    const content: Element = document.getElementById('export');
+
+    htmlPdf()
+      .from(content)
+      .set(options)
+      .save();
+  }
+
 }
